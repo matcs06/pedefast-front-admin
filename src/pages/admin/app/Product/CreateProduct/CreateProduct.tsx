2d5e6a3f786ba95d91f0f5f5fd43c61ec8050b9a
“matcs06” <you@example.com>
2023-07-05 11:30:06 -0300

    fixing acao

--- src/pages/admin/app/Product/CreateProduct/CreateProduct.tsx
+++ src/pages/admin/app/Product/CreateProduct/CreateProduct.tsx
@@ -143,7 +143,6 @@ export default function CreateProduct({ ...props }: ProductType) {
             await instace.post("/products", formData, {
                headers: {
                   "Content-Type": "multipart/form-data",
-                  "Access-Control-Allow-Origin": "*",
                   Authorization: "Bearer " + token,
                },
             });
