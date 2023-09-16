c52fdc8d87d5fabf81bc6e5ca2c44270167692bd
Mateus Costa Silva <38388657+matcs06@users.noreply.github.com>
2023-09-16 17:19:34 -0300

    Delete .next directory

--- .next/server/font-loader-manifest.js
+++
@@ -1,9 +0,0 @@
-self.__FONT_LOADER_MANIFEST={
-  "pages": {
-    "/_app": [
-      "static/media/0e4fe491bf84089c.p.woff2",
-      "static/media/934c4b7cb736f2a3.p.woff2"
-    ]
-  },
-  "app": {}
-}
